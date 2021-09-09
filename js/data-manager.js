eb749f242e407a89b5abf9dd1bdba18a45aa88d0
William <wsitu506@gmail.com>
2021-09-08 20:15:11 -0700

    Reorganize some functions of DataManager into a dictionary class

--- js/data-manager.js
+++ js/data-manager.js
@@ -24,9 +24,10 @@ class DataManager {
     }
     
     
-    addDictionary(dict) {
+    addDictionary(data) {
+        let dict = new WordDictionary(data);
+        dict.indexDictionary();
         this.dictionaries[dict.name] = dict;
-        this.indexDictionary(dict.name);
         let i = this.dictOrder.indexOf(dict.name);
         if (i != -1) this.dictOrder.splice(i, 1);
         this.dictOrder.push(dict.name);
@@ -34,7 +35,6 @@ class DataManager {
     
     removeDictionary(dictName) {
         delete this.dictionaries[dictName];
-        delete this.index[dictName];
         let i = this.dictOrder.indexOf(dictName);
         if (i != -1) this.dictOrder.splice(i, 1);
     }
@@ -51,44 +51,52 @@ class DataManager {
         for (let i = 0; i < dicts.length; i++)
             this.addDictionary(dicts[i]);
     }
+
+}
+
+class WordDictionary {
+    constructor(dict={name: "", words: {}}) {
+        this.name = dict.name;
+        this.words = dict.words;
+        this.index = {};
+    }
     
-    getIndex(dictName) {
-        return this.index[dictName];
+    getIndex() {
+        return this.index;
     }
     
     // Indexes every word in the dictionary by the kanjis used
     // * see indexWord for more details
-    // Input: <dictName> name of dictionary the word belongs to
-    //        <ignore> array of characters to ignore   
-    indexDictionary(dictName, ignore = []) {
-        let dict = this.getDictionary(dictName);
-        this.index[dict.name] = {};
-        for (const [word] of Object.entries(dict.words))
-            this.indexWord(dictName, word, ignore);
+    // Input: <ignore> array of characters to ignore   
+    indexDictionary(ignore = []) {
+        for (const [word] of Object.entries(this.words))
+            this.indexWord(word, ignore);
     }
     
     // Store an index of the word by the kanjis used in it
-    // Input: <dictName> name of dictionary the word belongs to
-    //        <word> the word to index
+    // Input: <word> the word to index
     //        <ignore> array of characters to ignore    
     /* For more flexibility this function indexes every character in a text
        with a non empty reading, not in <ignore>. If necessary, filter
        characters by kanji unicode value range instead. */
-    indexWord(dictName, word, ignore = []) {
-        let dict = this.getDictionary(dictName);
-        let wordParts = dict.words[word].part;
-        let index = this.getIndex(dictName);
+    indexWord(word, ignore = []) {
+        let wordParts = this.words[word].part;
         for (let i = 0; i < wordParts.length; i++) {
             let part = wordParts[i];
             if (!part.read) continue;
             for (let character of part.text) {
                 if (ignore.includes(character)) continue;
-                if (index[character])
-                    index[character].push(word);
+                if (this.index[character])
+                    this.index[character].push(word);
                 else
-                    index[character] = [word];
+                    this.index[character] = [word];
             }
         }
         
     }
+    
+    /*
+    toJSON() {
+    }
+    */
 }
