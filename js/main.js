3f4600b012ea6567bab4ae719d4c25e10ef82cba
William <wsitu506@gmail.com>
2022-07-07 00:29:22 -0700

    Load user data upon creating DataManager

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ main();
 function main() {
     let data = new DataManager();
     data.addDict(defaultWords, false);
-
+    if (data.hasUserStorage())
+        data.loadUserData();
     let quiz = new kanjiQuiz.Quiz(data);
     quiz.addTo(document.getElementsByTagName("body")[0]);
 }
