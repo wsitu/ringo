e9255b4dacb9774f7eeb066ab68116cbd40a5ad5
William <wsitu506@gmail.com>
2022-07-13 22:42:50 -0700

    Add createEl in WElement for only creating elements, shorten rootElement to root

--- js/ringo.js
+++ js/ringo.js
@@ -9,34 +9,36 @@ ringo.WElement = class {
     
     rootElement | the HTML element created by the constructor
     */
-    
-
-    /* <elConfig> element config with the following keys
-           tag:  (required) string of the type of html element to return
-           attr: (optional) object representing html attribute: value
-           text: (optional) string of the text content (not tml)
-           html: (optional) string of the html content (overwrites text)
-    */
     constructor(elConfig = {tag: "div", attr: {}, html: "", text: ""}) {
+        this.root = ringo.WElement.createEl(elConfig);
+    }
+    
+    /* <elConfig> element config with the following keys
+       tag:  (required) string of the type of html element to return
+       attr: (optional) object representing html attribute: value
+       text: (optional) string of the text content (not tml)
+       html: (optional) string of the html content (overwrites text)
+    */
+    static createEl(elConfig = {tag: "div", attr: {}, html: "", text: ""}) {
         let e = document.createElement(elConfig.tag);
         if (elConfig.text) e.textContent = elConfig.text;
         if (elConfig.html) e.innerHTML = elConfig.html;
         let attributes = elConfig.attr || {};
         for (const [key, value] of Object.entries(attributes))
             e.setAttribute(key, value);
-        this.rootElement = e;
+        return e;
     }
     
     // Appends this object as the last child to parentEl
     addTo(parentEl) {
-        let node = parentEl instanceof Node ? parentEl : parentEl.rootElement;
-        node.appendChild(this.rootElement);
+        let node = parentEl instanceof Node ? parentEl : parentEl.root;
+        node.appendChild(this.root);
     }
     
     // Appends childEl to this object as the last child
     addChild(childEl) {
-        let node = childEl instanceof Node ? childEl : childEl.rootElement;
-        this.rootElement.appendChild(node);
+        let node = childEl instanceof Node ? childEl : childEl.root;
+        this.root.appendChild(node);
     }
 }
 
