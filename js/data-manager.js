5f20bf9ff1a6d95b4db2619436ba736400d39e0e
William <wsitu506@gmail.com>
2022-07-26 19:44:11 -0700

    Improve WordDictionary's comments

--- js/data-manager.js
+++ js/data-manager.js
@@ -342,43 +342,38 @@ class DataManager {
 }
 
 class WordDictionary {
-    
-    /* Takes in another WordDictionary or object with name and words keys where:
-           name stores a string identifier for the dictionary
-           words stores an object { aWord: { part: {}, def: ""}, ... }
-           * for more info on part and def see WordData's parts and definition
-       The input is deep copied but if the input is another WordDictionary, the
-       _index is recreated as the words are added. Note that the aWord keys in
-       words is ignored and only the data is added. See this.add for more.
-    */
+    // Takes in another WordDictionary or object with keys where:
+    //     <name> a string identifier for the dictionary
+    //     <words> an object who's values are valid input for WordData's
+    //         constructor in the form of: { aWord: { part: {}, def: ""}, ... }
+    // * aWord key is used for searching within the file, it is ignored here
+    // * Input is deep copied except for _index which is remade on creation
     constructor(dict={name: "", words: {}}) {
+        // ! Apply any stored data changes to this.JSON() ! //
         this.name = dict.name;
         this._words = {}
         this._index = {};
-        // apply changes to this.JSON
         
         let words = (dict._words) ? dict._words : dict.words
         for (const data of Object.values(words))
             this.add(new WordData(data));
     }
 
-    // Returns a set of every word in the dictionary
+    // Returns a set of the strings, not WordData
     get words() {
         return new Set(Object.keys(this._words));
     }
     
-    // Returns true if the dictionary has an entry for wordString
     has(wordString) {
         return wordString in this._words;
     }
     
-    // Returns the WordData of wordString if found or undefined
+    // Returns the WordData or undefined if not found
     getData(wordString) {
         return this._words[wordString];
     }
     
-    // Adds the WordData to the dictionary located by its word text
-    // Overwrites previous entries with the same word text
+    // Overwrites previous entries with the same wordData.text
     add(wordData) {
         let word = wordData.text;
         if (this.has(word)) this.delete(word);
@@ -386,25 +381,23 @@ class WordDictionary {
         this._indexWord(word);
     }
     
-    // Removes the word and its data from the dictionary
     delete(wordString) {
         this._unindexWord(wordString);
         delete this._words[wordString];
     }
     
-    // Returns a set of kanji from every word in the dictionary or empty set
-    //  * see WordData.kanji() for more
+    // Retuns a set of all kanji used in the dictionary, see WordData.kanji()
     get kanjiList() {
         return new Set(Object.keys(this._index));
     }
     
-    // Returns a set of words that kanjiString is in or an empty set
+    // Returns a set of words (strings) that kanjiString is found in 
     wordsWith(kanjiString) {
         if (!this._index[kanjiString]) return new Set();
         return new Set(this._index[kanjiString]);
     }
 
-    // Store an index of the kanji in wordString 
+    // Adds the wordString to the index of kanji -> wordsUsingKanji
     _indexWord(wordString) {
         let data = this.getData(wordString);
         for (let character of data.kanji){
@@ -414,7 +407,6 @@ class WordDictionary {
         } 
     }
     
-    // Removes the index of wordString
     _unindexWord(wordString) {
         let data = this.getData(wordString);
         if (!data) return;
@@ -427,7 +419,7 @@ class WordDictionary {
         } 
     }
 
-    // This class is a wrapper for data so output as it was input
+    // Output the JSON as it would have been input
     toJSON() {
         return {name: this.name, words: this._words};
     }
